b9c696398babc99763564c8ac5d714a9bfe4e6ae
mohamed abdalla <almgromalmgroma@gmail.com>
2025-06-04 00:12:30 +0200

    updante the main page bootstrap link

--- Home/index.php
+++ Home/index.php
@@ -16,8 +16,8 @@
   <?php
   require_once('../includes/head/head.php');
   ?>
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"> -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="socialmediaicons.css">
